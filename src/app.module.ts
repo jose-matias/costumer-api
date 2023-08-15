200b305e2cfc8b94ae2bc12d87477e8c2c543216
José Matias <josematias.dev@gmail.com>
2023-08-14 22:28:14 -0300

    chore: imports modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+import { AuthModule } from '@auth/auth.module';
+import { RedisModule } from '@config/cache/redis.module';
+import { CustomerModule } from '@modules/customer/customer.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    RedisModule,
+    CustomerModule,
+  ],
+})
 export class AppModule { }
